a8fd1b1e328fd4ac7e8035e9db3c2b49735fefef
bpyxh <no-email@1.com>
2025-08-02 21:33:50 +0800

    调整：编译错误

--- server/src/msfs/FileManager.cpp
+++ server/src/msfs/FileManager.cpp
@@ -227,7 +227,8 @@ void FileManager::updateMapCache() {
 	size_t currSize = m_map.size();
 	if (currSize > MAX_FILE_IN_MAP) {
 		EntryMap::iterator it = m_map.begin();
-		int times = abs(MAX_FILE_IN_MAP - currSize);
+		// int times = abs(MAX_FILE_IN_MAP - currSize);
+        int times = abs(static_cast<long long int>(MAX_FILE_IN_MAP - currSize))
 		while (it != m_map.end() && times) {
 			delete it->second;
 			m_map.erase(it++);
