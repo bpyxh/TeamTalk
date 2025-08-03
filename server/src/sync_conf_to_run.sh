3adf51e7e4b2c80178e108e07c297a094418d319
bpyxh <no-email@1.com>
2025-08-03 14:23:14 +0800

    调整：拷贝conf脚本

---
+++ server/src/sync_conf_to_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cp ../../auto_setup/im_server/conf/loginserver.conf ../run/login_server/
+
+cp ../../auto_setup/im_server/conf/routeserver.conf ../run/route_server/
+
+cp ../../auto_setup/im_server/conf/msgserver.conf ../run/msg_server/
+
+cp ../../auto_setup/im_server/conf/httpmsgserver.conf ../run/http_msg_server/
+
+cp ../../auto_setup/im_server/conf/fileserver.conf ../run/file_server/
+
+cp ../../auto_setup/im_server/conf/pushserver.conf ../run/push_server/
+
+cp ../../auto_setup/im_server/conf/dbproxyserver.conf ../run/db_proxy_server/
+
+cp ../../auto_setup/im_server/conf/msfs.conf ../run/msfs/
