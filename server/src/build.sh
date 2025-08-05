24adefabf3464efd6d7301ba07dd4d350e3a7eae
bpyxh <no-email@1.com>
2025-08-05 11:26:16 +0800

    调整：安装这些库需要root权限，单独放一个文件里。让build.sh不用root权限也能够执行

--- server/src/build.sh
+++ server/src/build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 build() {
-    yum -y install cmake
-    yum -y install libuuid-devel
-    yum -y install openssl-devel
-    yum -y install curl-devel
-
 	echo "#ifndef __VERSION_H__" > base/version.h
 	echo "#define __VERSION_H__" >> base/version.h
 	echo "#define VERSION \"$1\"" >> base/version.h
---
+++ server/src/make_dev_lib.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yum -y install cmake
+yum -y install libuuid-devel
+yum -y install openssl-devel
+yum -y install curl-devel
+
